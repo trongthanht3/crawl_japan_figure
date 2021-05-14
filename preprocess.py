3331fc91df199ef2d8129b215240315f62fec641
trongthanht3 <trongthanht3@gmail.com>
2021-05-14 13:35:13 +0700

    clean data

--- preprocess.py
+++ preprocess.py
@@ -2,4 +2,20 @@ import pandas as pd
 import re
 
 df = pd.read_csv('data/final/data.csv', encoding='utf-8')
-print(df.head)
+
+df_pub = pd.read_csv('data/final/pubs.csv')
+
+df_pub_single = df_pub.drop_duplicates(subset=['publisher'])
+df_pub_single.reset_index(inplace=True)
+df_pub_single.drop(['index'], axis=1, inplace=True)
+df_pub_single['id'] = df_pub_single.index
+
+df_pub_single.to_csv('data/final/df_pub_proced.csv', encoding='utf-8')
+
+for id_item, pub_item in zip(df['id'], df_pub['publisher']):
+  for id_pub, pub_name in zip(df_pub_single['id'], df_pub_single['publisher']):
+    if pub_item == pub_name:
+      df.loc[df['id'] == id_item, 'idPublisher'] = int(id_pub)
+
+df['idPublisher'] = pd.to_numeric(df['idPublisher'], downcast='integer')
+df.to_csv('data/final/data_proced.csv', encoding='utf-8')
